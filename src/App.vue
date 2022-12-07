a994f302a4db2d6d9b68b2340b0e665804f0a281
JaeKwan Ahn <52008@naver.com>
2022-12-07 21:28:25 +0900

    Add router

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,9 @@
 <template>
-  <h1>
-    {{ msg }}
-  </h1>
-  <div />
-  <Hello />
+  <RouterView />
 </template>
 
 <script>
-import Hello from "~/components/Hello";
-
 export default {
-  components: {
-    Hello,
-  },
-  data() {
-    return {
-      msg: "Hello Vue!",
-    };
-  },
-};
+  
+}
 </script>
--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,20 @@
-const path = require("path");
-const { VueLoaderPlugin } = require("vue-loader");
-const HtmlPlugin = require("html-webpack-plugin");
-const CopyPlugin = require("copy-webpack-plugin");
+const path = require('path')
+const { VueLoaderPlugin } = require('vue-loader')
+const HtmlPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
 
 module.exports = {
   resolve: {
-    extensions: [".vue", ".js"],
+    extensions: ['.vue', '.js'],
     alias: {
       // 루트 -> src를 찾는다.
-      "~": path.resolve(__dirname, "src"),
+      '~': path.resolve(__dirname, 'src'),
     },
   },
-  entry: "./src/main.js",
+  entry: './src/main.js',
   output: {
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, 'dist'),
+    publicPath:'/',
     clean: true,
     // filename: "",
   },
@@ -21,12 +22,12 @@ module.exports = {
     rules: [
       {
         test: /\.vue$/,
-        use: "vue-loader",
+        use: 'vue-loader',
       },
       {
         // 제일 먼저 평가되어야 하는 로더가 제일 나중에 작성되야 한다.
         test: /\.s?css$/,
-        use: ["vue-style-loader", "css-loader", "sass-loader"],
+        use: ['vue-style-loader', 'css-loader', 'sass-loader'],
       },
     ],
   },
@@ -34,10 +35,13 @@ module.exports = {
     new VueLoaderPlugin(),
     new HtmlPlugin({
       // 기본적으로 resolve가 적용되어 있다.
-      template: "./src/index.html",
+      template: './src/index.html',
     }),
     new CopyPlugin({
-      patterns: [{ from: "static" }],
+      patterns: [{ from: 'static' }],
     }),
   ],
-};
+  devServer: {
+    historyApiFallback:true
+  }
+}
