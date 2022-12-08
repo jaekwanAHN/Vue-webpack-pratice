ae2668e53b7a512dc2266b5382db856e905d40d9
JaeKwan Ahn <52008@naver.com>
2022-12-08 11:43:44 +0900

    Fix: 오타수정

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,12 @@
 <template>
-  <RouterView />
+  <div class="app_inner">
+    <div class="app__nav"></div>
+    <div class="app__page">
+      <div class="page__header"></div>
+      <div class="page__container"></div>
+        <RouterView/>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -7,3 +14,28 @@ export default {
   
 }
 </script>
+
+<style lang="scss" scoped>
+
+* {
+  border: 4px solid;
+  box-sizing: border-box;
+}
+.app__inner {
+  display: flex;
+  height: 100vh;
+  .app__nav{
+    width: 240px;
+    height: 100%;
+    .app__page{
+      height: 100%;
+      .page__header{
+        height:80px;
+      }
+      .page__container{
+
+      }
+    }
+  }
+}
+</style>
